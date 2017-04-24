c27300ecde33697a12e4256d1c1f24b7bc712ed2
radjah31 <radjahamin@gmail.com>
2017-04-24 22:00:04 +0100

    first app
    app about C Shrp

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // This is my first app in CSharp in GitHub
+            Console.WriteLine("Hello Word !");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 24;
+            Console.WriteLine("x = {0}", x);
+            Console.ReadKey();
+        }
+    }
+}
